356385d5eca91c49ec6160477727bd3c79b0cac2
cyub <cosmicy@outlook.com>
2016-08-04 08:33:53 +0800

    init

---
+++ app/src/main/java/com/example/cy/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.cy.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //隐藏标题栏
+        getSupportActionBar().hide();
+
+        btn1 = (Button) findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "发生地方", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+    }
+}
