6cda883b53e86c74f8aea1fabc9a5d74cb13a9ba
cyub <cosmicy@outlook.com>
2016-08-11 14:34:27 +0800

    *Lib中添加计算方法

---
+++ mylibrary/src/main/java/com/example/mylibrary/CalcUtil.java
@@ -0,0 +1,15 @@
+package com.example.mylibrary;
+
+/**
+ * Created by cy on 16-8-11.
+ */
+
+public class CalcUtil {
+
+    public static int add(int a, int b) {
+        int ret = a + b * 3;
+        return ret;
+    }
+
+
+}
